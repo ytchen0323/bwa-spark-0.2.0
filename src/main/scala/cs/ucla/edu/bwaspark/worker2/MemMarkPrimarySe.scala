62368f6f29e825d73c5e54c7a997c04fa51a05d9
AresLL <memoryzpp@gmail.com>
2014-04-17 21:18:47 -0700

    MemMarkPrimarySe finished

--- src/main/scala/cs/ucla/edu/bwaspark/worker2/MemMarkPrimarySe.scala
+++ src/main/scala/cs/ucla/edu/bwaspark/worker2/MemMarkPrimarySe.scala
@@ -23,6 +23,7 @@ object MemMarkPrimarySe {
   def memMarkPrimarySe(opt: MemOptType, n: Int, a: List[MemAlnRegType], id: Long) {
     var i: Int = 0
     var j: Int = 0
+    var tmp: Int = 0
     var k: Int = 0
     var z: MutableList[Int] = new MutableList
 
@@ -36,12 +37,37 @@ object MemMarkPrimarySe {
       //ks_introsort(mem_ars_hash, n, a)
       //#define alnreg_hlt(a, b) ((a).score > (b).score || ((a).score == (b).score && (a).hash < (b).hash))
       a.sortWith( (x, y) => ((x.score > y.score) || ( x.score == y.score && x.hash < y.hash)  ) )
-
-	
-
+      tmp = opt.a + opt.b
+      if((opt.oDel + opt.eDel) > tmp) {
+	tmp = opt.oDel + opt.eDel
+      }
+      if((opt.oIns + opt.eIns) > tmp) {
+	tmp = opt.oIns + opt.eIns
+      }
+      //kv_push()
+      z += 0
+      for(i <- 1 until n) {
+	breakable {
+	  for(k <- 0 until z.size) {
+	    j = z(k)
+	    var bMax: Int = if(a(j).qBeg > a(i).qBeg) a(j).qBeg else a(i).qBeg
+	    var eMin: Int = if(a(j).qEnd < a(i).qEnd) a(j).qEnd else a(i).qEnd
+	    // have overlap
+	    if( eMin > bMax ) {
+	      var minL: Int = if ((a(i).qEnd - a(i).qBeg)<(a(j).qEnd - a(j).qBeg)) (a(i).qEnd - a(i).qBeg) else (a(j).qEnd - a(j).qBeg)
+	      //have significant overlap
+	      if((eMin - bMax)>= minL * opt.maskLevel) {
+		if(a(j).sub == 0) a(j).sub = a(i).score
+		if((a(j).score - a(i).score) <= tmp) a(j).subNum = a(j).subNum + 1
+		break
+	      }
+	    }
+	  }
+	  if(k == z.size) z += i
+	  else a(i).secondary = z(k)
+	}
+      }
     }
-
-
   }
 
   private def hash64( key: Long ) : Long = {
