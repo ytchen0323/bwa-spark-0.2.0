8dd6ae15c1a297cd3a762f219dd026429e18fe6c
Yu-Ting Chen <heeros@gmail.com>
2014-04-19 20:00:35 -0700

    memApproxMapqSe.scala implemented not tested yet

--- src/main/scala/cs/ucla/edu/bwaspark/worker2/MemRegToADAMSAM.scala
+++ src/main/scala/cs/ucla/edu/bwaspark/worker2/MemRegToADAMSAM.scala
@@ -3,11 +3,15 @@ package cs.ucla.edu.bwaspark.worker1
 import scala.util.control.Breaks._
 import scala.List
 import scala.collection.mutable.MutableList
+import scala.math.log
 
 import cs.ucla.edu.bwaspark.datatype._
 
 object MemRegToADAMSAM {
   val MEM_F_ALL = 0x8
+  val MEM_F_NO_MULTI = 0x10
+  val MEM_MAPQ_COEF = 30.0
+
   /**
     *  Transform the alignment registers to SAM format
     *  
@@ -15,12 +19,96 @@ object MemRegToADAMSAM {
     *  @param bns the input BNSSeqType object
     *  @param pac the PAC array
     *  @param seq the read (NOTE: currently we use Array[Byte] first. may need to be changed!!!)
-    *  @param regsIn the alignment registers to be transformed
+    *  @param regs the alignment registers to be transformed
     *  @param extraFlag
     *  @param alns 
     */
-  def memRegToSAMSe(opt: MemOptType, bns: BNTSeqType, pac: Array[Byte], seq: Array[Byte], regsIn: MutableList[MemAlnRegType], extraFlag: Int, alns: List[MemAlnReg]) {
-    var regs = regsIn.filter(_.score >= opt.T).filter(r => (r.secondary < 0) || ((opt.flag & MEM_F_ALL) > 0))
+  def memRegToSAMSe(opt: MemOptType, bns: BNTSeqType, pac: Array[Byte], seq: Array[Byte], regs: MutableList[MemAlnRegType], extraFlag: Int, alnsIn: MutableList[MemAlnType]) {
+    var alns: MutableList[MemAlnType] = new MutableList[MemAlnType]
+
+    for(i <- 0 to (regs.length - 1)) {
+      if(regs(i).score >= opt.T) {
+        if(regs(i).secondary < 0 || ((opt.flag & MEM_F_ALL) > 0)) {
+          if(regs(i).secondary < 0 || regs(i).score >= regs(regs(i).secondary).score * 0.5) {
+            var aln = memRegToAln(opt, bns, pac, 101, seq, regs(i))   // NOTE: current data structure has not been obtained from RDD. We assume the length to be 101 here
+            alns += aln
+
+            aln.flag |= extraFlag   // flag secondary
+            if(regs(i).secondary >= 0) aln.sub = -1   // don't output sub-optimal score
+            if(i > 0 && regs(i).secondary < 0)   // if supplementary
+              if((opt.flag & MEM_F_NO_MULTI) > 0) aln.flag |= 0x10000
+              else aln.flag |= 0x800
+
+            if(i > 0 && aln.mapq > alns(0).mapq) aln.mapq = alns(0).mapq            
+          }
+        }
+      }
+    }
+
+    // no alignments good enough; then write an unaligned record
+    if(alns.length == 0) { 
+      var aln = memRegToAln(opt, bns, pac, 101, seq, null)
+      aln.flag |= extraFlag
+      //memAlnToSAM
+    }
+    else {
+      //alns.foreach(memAlnToSAM)
+    }
   }
+
+
+  /**
+    *  Transform the alignment registers to alignment type
+    *
+    *  @param opt the input MemOptType object
+    *  @param bns the input BNSSeqType object
+    *  @param pac the PAC array
+    *  @param seqLen the length of the input sequence
+    *  @param seq the input sequence
+    *  @param reg the input alignment register
+    */
+  private def memRegToAln(opt: MemOptType, bns: BNTSeqType, pac: Array[Byte], seqLen: Int, seq: Array[Byte], reg: MemAlnRegType): MemAlnType = {
+    val aln = new MemAlnType
+    aln
+  }
+
+
+  private def memApproxMapqSe(opt: MemOptType, reg: MemAlnRegType): Int = {
+    var sub = 0
+    
+    if(reg.sub > 0) sub = reg.sub
+    else sub = opt.minSeedLen * opt.a
+
+    if(reg.csub > sub) sub = reg.csub
+   
+    if(sub >= reg.score) 0
+    else {
+      var len = 0
+      var mapq = 0
+      
+      if(reg.qEnd - reg.qBeg > reg.rEnd - reg.rBeg) len = reg.qEnd - reg.qBeg
+      else len = (reg.rEnd - reg.rBeg).toInt
+      
+      val identity = 1.0 - (len * opt.a - reg.score) / (opt.a + opt.b) / len
+      if(reg.score == 0) mapq = 0
+      else if(opt.mapQCoefLen > 0) {
+        var tmp: Double = 1.0
+        if(len > opt.mapQCoefLen) tmp = opt.mapQCoefFac / log(len)
+        tmp *= identity * identity
+        mapq = (6.02 * (reg.score - sub) / opt.a * tmp * tmp + 0.499).toInt
+      }
+      else {
+        mapq = (MEM_MAPQ_COEF * (1.0 - sub.toDouble / reg.score) * log(reg.seedCov) + .499).toInt
+        if(identity < 0.95) mapq = (mapq * identity * identity + .499).toInt
+      }
+   
+      if(reg.subNum > 0) mapq -= (4.343 * log(reg.subNum + 1) + .499).toInt
+      if(mapq > 60) mapq = 60
+      if(mapq < 0) mapq = 0
+
+      mapq
+    }
+
+  }  
 }
 
