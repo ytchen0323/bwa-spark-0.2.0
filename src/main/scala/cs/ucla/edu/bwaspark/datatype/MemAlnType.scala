c23f339ec8c028aef55e08fb6516951c0cfddf69
Yu-Ting Chen <heeros@gmail.com>
2014-04-24 16:48:47 -0700

    MemAlnType.scala updated

--- src/main/scala/cs/ucla/edu/bwaspark/datatype/MemAlnType.scala
+++ src/main/scala/cs/ucla/edu/bwaspark/datatype/MemAlnType.scala
@@ -4,7 +4,7 @@ class MemAlnType {
   var pos: Long = 0          // forward strand 5'-end mapping position
   var rid: Int = 0           // reference sequence index in bntseq_t; <0 for unmapped
   var flag: Int = 0          // extra flag
-  var isRev: Boolean = false // is_rev: whether on the reverse strand
+  var isRev: Byte = 0        // is_rev: whether on the reverse strand
   var mapq: Short = 0        // mapq: mapping quality
   var NM: Int = 0            // NM: edit distance
   var nCigar: Int = 0        // number of CIGAR operations
